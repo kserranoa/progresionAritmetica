231b14f8a05eb0bef987375cad2bda19ba035bae
Karina <kserranoa16@gmail.com>
2021-01-28 16:18:35 -0600

    Lista reverse y count

--- lista.py
+++ lista.py
@@ -6,5 +6,15 @@ prod = [['arroz', 1500, 1], ['lechuga', 500, 3], ['tomate', 1000, 3]]
 letras = ['a', 'b', 'c', 'a']
 if 'a' in letras:
     print("i")
+    
 #Saber cuantas veces
 print(letras.count('a'))
+
+#Reverse
+letras.reverse()
+print(letras)
+
+#Acceder elementos en orden contrario
+so = ['Windows', 'MacOs', 'Linux']
+for o in reversed(so):
+    print(o)
