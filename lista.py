80fe2e421298d64ba09227c68f4abc3190010855
Karina <kserranoa16@gmail.com>
2021-01-28 18:59:49 -0600

    Inicio del juego

--- lista.py
+++ lista.py
@@ -1,20 +1,8 @@
-prod = [['arroz', 1500, 1], ['lechuga', 500, 3], ['tomate', 1000, 3]]
+catalogo = ['hamburguesa', 'papas', 'camote', 'helado']
+inventario = [['', '']]
+factura = [['', '', '']]
 
-#print([i*3 for i in prod])
+for i in enumerate(catalogo):
+    print("Opcion", i)
 
-#print(prod[0+1][0])
-letras = ['a', 'b', 'c', 'a']
-if 'a' in letras:
-    print("i")
-    
-#Saber cuantas veces
-print(letras.count('a'))
-
-#Reverse
-letras.reverse()
-print(letras)
-
-#Acceder elementos en orden contrario
-so = ['Windows', 'MacOs', 'Linux']
-for o in reversed(so):
-    print(o)
+opcion = input("Ingrese opcion")
